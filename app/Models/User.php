583bed9ac6ec36e50c0d262b7c4fa5ca143c1661
Jdexurbano <dexterurbano25@gmail.com>
2025-01-19 23:41:16 +0800

    add relationship to announcement model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Announcement;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -53,5 +54,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function announcement(){
+        return $this->hasMany(Announcement::class);
     }
 }
