233c01adf2efc61684ffbb45eaf5b7f20f18800a
Jdexurbano <dexterurbano25@gmail.com>
2025-01-19 23:42:38 +0800

    add routes for announcements

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AnnouncementController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -20,4 +21,5 @@ Route::post('/student',[AuthController::class,'studentLogin']);
 
 Route::middleware('auth:sanctum')->group(function (){
     Route::apiResource('students',UserController::class);
+    Route::apiResource('announcements',AnnouncementController::class);
 });
